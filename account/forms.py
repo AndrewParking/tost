fa454ec9d84432e0e6ce82f445daa20aa4a68925
AndrewParking <popow.andrej2009@yandex.ru>
2015-09-13 16:57:33 +0300

    CreateAccountForm done and tested

--- account/forms.py
+++ account/forms.py
@@ -1,1 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import Account
 
+
+class CreateAccountForm(UserCreationForm):
+
+    class Meta:
+        model = Account
+        fields = (
+            'username',
+            'email',
+            'tagline',
+            'description',
+        )
--- account/test_forms.py
+++ account/test_forms.py
@@ -1,1 +1,37 @@
-__author__ = 'andrew'
+from django.test import TestCase
+from .forms import CreateAccountForm
+
+
+class CreateAccountFormTest(TestCase):
+
+    def test_with_empty_data(self):
+        form = CreateAccountForm({})
+        self.assertFalse(form.is_valid())
+
+    def test_with_valid_data(self):
+        form = CreateAccountForm({
+            'username': 'Andrew',
+            'email': 'pop@tut.by',
+            'tagline': 'We don\'t saw!',
+            'password1': 'homm1994',
+            'password2': 'homm1994'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_with_invalid_data(self):
+        form = CreateAccountForm({
+            'username': 'A'*150,
+            'email': 'pop@tut.by',
+            'password1': 'homm1994',
+            'password2': 'homm1994'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_with_different_passwords(self):
+        form = CreateAccountForm({
+            'username': 'Andrew',
+            'email': 'pop@tut.by',
+            'password1': 'homm1994',
+            'password2': 'homm1995'
+        })
+        self.assertFalse(form.is_valid())
